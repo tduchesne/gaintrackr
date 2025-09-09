9a89b452fa2bc52c061ca40633cc8aadaf3d4781
Thierry Duchesne <thierryduchesne95@hotmail.com>
2025-09-09 18:17:48 -0400

    feat: Implement basic console menu
    - Implemented a basic console menu with a main loop and a switch statement.
    - Added a basic display feature to show the sample workout program.

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 /**
  * Hello world!
  *
@@ -6,26 +8,54 @@ public class App
 {
     public static void main( String[] args )
     {
-
         System.out.println("Lancement de l'application");
-        Mesocycle mesocycle = WorkoutFactory.createSampleMesocycle();
+        Scanner scanner = new Scanner(System.in);
 
-        System.out.println(mesocycle.name());
-        System.out.println("==============");
-        for(Week week : mesocycle.weeks()){
-            System.out.println("Week " + week.weekNumber());
-            for(Day day : week.days()){
-                System.out.println("\t" + day.name());
-                for(Exercise exercise : day.exercises()) {
-                    System.out.println("\t\t" + exercise.name());
-                    for(Set set : exercise.sets()) {
-                        System.out.print("\t\t\t Reps : " + set.reps());
-                        System.out.println(" - Load : " +  set.load());
+        // Menu principal
+        while(true){
+            System.out.println("Veuillez choisir une option: ");
+            System.out.println("1. Afficher le programme d'entraînement");
+            System.out.println("2. Ajouter une séance d'entraînement");
+            System.out.println("3. Quitter l'application");
+
+            int input = scanner.nextInt();
+            // TODO vérifier l'entrée
+
+            switch(input) {
+                case 1:
+                    Mesocycle mesocycle = WorkoutFactory.createSampleMesocycle();
+
+                    System.out.println(mesocycle.name());
+                    System.out.println("==============");
+                    for(Week week : mesocycle.weeks()){
+                        System.out.println("Week " + week.weekNumber());
+                        for(Day day : week.days()){
+                            System.out.println("\t" + day.name());
+                            for(Exercise exercise : day.exercises()) {
+                                System.out.println("\t\t" + exercise.name());
+                                for(Set set : exercise.sets()) {
+                                    System.out.print("\t\t\t Reps : " + set.reps());
+                                    System.out.println(" - Load : " +  set.load());
+                                }
+
+                            }
+                        }
                     }
+                    break;
+                case 2:
+                    System.out.println("Ajoute une séance - TODO");
+                    break;
+                case 3:
+                    System.exit(0);
+                default:
+                    System.out.println("Erreur, veuillez choisir une option valide.");
 
-                }
             }
+
         }
+
+
+
 
     }
 }
